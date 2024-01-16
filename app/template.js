02af501d6d08572a74c6569213f2bdd7292d192a
Webkit <yasingenc93@gmail.com>
2024-01-16 14:06:17 -0800

    template updates

--- app/template.js
+++ app/template.js
@@ -1,38 +1,64 @@
 'use client';
 
 import React, { useEffect, useRef } from 'react';
-import styles from './Template.module.scss';
 
 export default function Template({ children }) {
+    const styles = {
+        loader: {
+            height: 'calc(100vh + 200px)',
+            width: '100%',
+            position: 'fixed',
+        },
+        svg: {
+            height: '100%',
+            width: '100%',
+        },
+        path: {
+            stroke: 'black',
+            strokeWidth: '1px'
+        }
+    }
 
     const loader = useRef(null);
     const path = useRef(null);
     const initialCurve = 200;
     const duration = 600;
-    let start;
+    const startRef = useRef(); // Create a useRef for 'start'
 
     useEffect(() => {
-        setPath(initialCurve)
-        setTimeout( () => {
-            requestAnimationFrame(animate)
-        }, 500)
-    }, [])
+        const animate = (timestamp) => {
+            if (startRef.current === undefined) {
+                startRef.current = timestamp; // Update 'start' using the useRef
+            }
+            const elapsed = timestamp - start;
 
-    const animate = (timestamp) => {
-        if(start === undefined){
-            start = timestamp
+            const newCurve = easeOutQuad(elapsed, initialCurve, -200, duration);
+            setPath(newCurve);
+
+            loader.current.style.top = easeOutQuad(elapsed, 0, -loaderHeight(), duration) + "px";
+
+            if (elapsed < duration) {
+                requestAnimationFrame(animate);
+            }
         }
-        const elapsed = timestamp - start;
 
-        const newCurve = easeOutQuad(elapsed, initialCurve, -200, duration)
-        setPath(newCurve);
-
-        loader.current.style.top = easeOutQuad(elapsed, 0, -loaderHeight(), duration) + "px";
-
-        if(elapsed < duration){
-            requestAnimationFrame(animate)
+        const setPath = (curve) => {
+            const width = window.innerWidth
+            const height = loaderHeight();
+            path.current.setAttributeNS(null, "d",
+                `M0 0
+                L${width} 0
+                L${width} ${height}
+                Q${width / 2} ${height - curve} 0 ${height}
+                L0 0`
+            );
         }
-    }
+
+        setPath(initialCurve);
+        setTimeout(() => {
+            requestAnimationFrame(animate);
+        }, 500);
+    }, []); // Include 'animate' and 'setPath' in the dependency array
 
     const easeOutQuad = (time, start, end, duration) => {
         return -end * (time /= duration) * (time - 2) + start;
@@ -43,23 +69,11 @@ export default function Template({ children }) {
         return loaderBounds.height;
     }
 
-    const setPath = (curve) => {
-        const width = window.innerWidth
-        const height = loaderHeight();
-        path.current.setAttributeNS(null, "d",
-            `M0 0
-    L${width} 0
-    L${width} ${height}
-    Q${width/2} ${height - curve} 0 ${height}
-    L0 0`
-        )
-    }
-
     return (
         <div>
             <div ref={loader} className={styles.loader}>
-                <svg>
-                    <path ref={path}></path>
+                <svg className={styles.svg}>
+                    <path className={styles.path} ref={path}></path>
                 </svg>
             </div>
 
