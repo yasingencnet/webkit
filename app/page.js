74b693831fd401bbfd7a0c60edd4e2596f2ffbac
Webkit <yasingenc93@gmail.com>
2024-01-19 17:24:18 -0800

    fffsss

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     return (
         <>
             <Hero/>
-            {/*<SelectedWorks/>*/}
+            <SelectedWorks/>
             <SkillSet/>
             <ExperienceBlock/>
             <Gallery/>
